e384ba3c1ae9cda5a1bd0867cec011382c37a6be
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-27 09:13:40 +0530

    completed login

--- amrita-events/src/main.js
+++ amrita-events/src/main.js
@@ -1,24 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-
+ 
+import 'bootstrap/dist/css/bootstrap.min.css'
+ 
 Vue.config.productionTip = false
-import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import axios from 'axios';
-Vue.prototype.$http = axios;
-
-Vue.use(BootstrapVue)
-
-
-/* eslint-disable no-new */
+ 
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App)
+}).$mount('#app')
--- amrita-events/src/router/index.js
+++ amrita-events/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
-Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+import VueRouter from 'vue-router'
+ 
+Vue.use(VueRouter)
+ 
+  const routes = [
+  {
+    path: '/',
+    name: 'signup',
+    component: () => import('../components/Registration.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/Login.vue')
+  },
+  {
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: () => import('../components/ForgotPassword.vue')
+  }
+]
+ 
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+ 
+export default router
