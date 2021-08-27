1a77b1bb09638fcbce976ba6864fe317422a5a83
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-27 09:31:15 +0530

    eslint-stuff

--- amrita-events/src/components/login.vue
+++ amrita-events/src/components/login.vue
@@ -33,7 +33,7 @@
  
 <script>
     export default {
-        data() {
+        data () {
             return {}
         }
     }
--- amrita-events/src/components/registration.vue
+++ amrita-events/src/components/registration.vue
@@ -30,7 +30,7 @@
  
 <script>
     export default {
-        data() {
+        data () {
             return {}
         }
     }
